602386506be983c5ca07f84be7df85305696baed
Nhat Truong <truongphannhat90@phannhat90gmail.com>
2026-01-31 03:24:47 +0700

    Update LinkedList

---
+++ BienSoXe/include/LinkedList.h
@@ -0,0 +1,31 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "LicensePlate.h"
+
+struct Node {
+    LicensePlate data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+    
+    Node* mergeSort(Node* h);
+    Node* merge(Node* a, Node* b);
+    void split(Node* source, Node** front, Node** back);
+
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void insert(LicensePlate plate);
+    bool find(string plateStr);
+    void sort(); 
+    void displayByProvince(string province);
+    void displayByDistrict(string district);
+};
+
+#endif
